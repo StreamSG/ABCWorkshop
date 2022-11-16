1a6ac440936bd278d282e89670a089ba048533e0
micah.wehrle <micah23@gmail.com>
2022-11-16 13:22:30 -0500

    Added validation to ticket gen forms

--- src/app/components/ticket-generator/ticket-generator.component.ts
+++ src/app/components/ticket-generator/ticket-generator.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SeededBanDataGenerator, SeededPLEGenerator } from 'src/app/models/seeded-random-number.model';
 
 @Component({
@@ -9,51 +9,61 @@ import { SeededBanDataGenerator, SeededPLEGenerator } from 'src/app/models/seede
 })
 export class TicketGeneratorComponent implements OnInit {
 
-  public banForm!: FormGroup;
-  public seededBan!: SeededBanDataGenerator;
-  public customerData!: {
+  public banForm!: FormGroup; // The form for inputting a ban
+  public seededBan!: SeededBanDataGenerator; // Allows ban data to be generated from a ban seed
+  public customerData!: { // To store the generated data, so that it doesn't need to be recalculated each time it's bound in the dom
     address: string,
     phone: string,
     circuitID: string,
   };
 
-  public trainingForm!: FormGroup;
-  public seededTraining!: SeededPLEGenerator;
-  public trainingNumber!: number;
-  public trainingDue!: string[];
+  public trainingForm!: FormGroup; // The form for inputting a uuid
+  public seededTraining!: SeededPLEGenerator; // Allows courses to be generated from a uuid seed
+  public trainingNumber!: number; // Number of trainings due
+  public trainingDue!: string[]; // Array of training titles
 
   constructor() {}
   
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.banForm = new FormGroup({
-      'ban': new FormControl(null, [Validators.required, Validators.minLength(9), Validators.maxLength(9), Validators.pattern(/^[0-9]+$/gm)]),
+      'ban': new FormControl(null, [
+        Validators.required, 
+        Validators.minLength(9), 
+        Validators.maxLength(9), 
+        Validators.pattern(/^[0-9]+$/m) // Only allow numbers
+      ]),
     });
 
     this.trainingForm = new FormGroup({
-      'uuid': new FormControl(null, [Validators.required, Validators.minLength(6), Validators.maxLength(6), Validators.pattern(/[a-z]+[a-z]+[0-9]+[0-9]+[0-9]+[a-z0-9]/g)]),
-    })
-  }
-
-  onRandomBan() {
-    this.banForm.patchValue({
-      'ban': Math.floor(Math.random()*224000000)+100000000
+      'uuid': new FormControl(null, [
+        Validators.required, 
+        Validators.minLength(6), 
+        Validators.maxLength(6), 
+        Validators.pattern(/[a-z]+[a-z]+[0-9]+[0-9]+[0-9]+[a-z0-9]/) // only allow 2 letters, 3 numbers, and then a letter OR number
+      ]), 
     });
   }
 
-  onBanFormSubmit() {
+  public onRandomBan(): void {
+    this.banForm.patchValue({
+      'ban': ''+(Math.floor(Math.random()*224000000)+100000000) // generate a random ban between 100-000-000 and 323-999-999
+    });
+  }
+
+  public onBanFormSubmit(): void {
     const banData = this.banForm?.get('ban')?.value;
     if(banData && this.banForm.valid) {
-      this.seededBan = new SeededBanDataGenerator(+banData);
-      this.customerData = {
-        address: this.seededBan.getAddress(),
+      this.seededBan = new SeededBanDataGenerator(+banData); // create seeded random number generator
+      this.customerData = { // create and store address, phone, and circuit ID to be displayed in DOM
+        address: this.seededBan.getAddress(), 
         phone: this.seededBan.getPhoneNumber(),
         circuitID: this.seededBan.getCircuitID(),
       }
     }
   }
 
-  onRandomUUID() {
-    let genID = '';
+  public onRandomUUID(): void {
+    let genID = ''; // generate a random ban: 2 letters, 3 numbers, and a letter or number
     genID += String.fromCharCode(Math.floor(Math.random()*26)+97);
     genID += String.fromCharCode(Math.floor(Math.random()*26)+97);
     genID += Math.floor(Math.random()*10);
@@ -61,14 +71,14 @@ export class TicketGeneratorComponent implements OnInit {
     genID += Math.floor(Math.random()*10);
 
     const lastChar = Math.floor(Math.random()*36)+97;
-    genID += lastChar > 122 ? lastChar-122 : String.fromCharCode(lastChar);
+    genID += lastChar > 122 ? lastChar-122 : String.fromCharCode(lastChar); // if the ascii code is past z, return the ascii code as a number (0-9). Otherwise convert ascii code to letter
 
     this.trainingForm.patchValue({
       'uuid': genID,
     });
   }
 
-  onTrainingFormSubmit() {
+  public onTrainingFormSubmit(): void {
     const uuidData = this.trainingForm?.get('uuid')?.value;
     if(uuidData && this.trainingForm.valid) {
       this.seededTraining = new SeededPLEGenerator(uuidData);
