f1676ebe88e1e08fcf15593f4d8cc7c43ed74c3a
Kerry Kreiter <wonproudamerican@yahoo.com>
2022-11-29 22:56:15 -0600

    added ngIf to office-info template line 2

--- src/app/components/touch-n-go/t-n-gFlows/flow-wt/flow-wt.component.ts
+++ src/app/components/touch-n-go/t-n-gFlows/flow-wt/flow-wt.component.ts
@@ -6,13 +6,21 @@ import { ChangeDetectionStrategy, Component, Input, OnChanges,SimpleChanges } fr
   <div class="left text-center">
     <h2>Wire Tech</h2>
     <p>Status: {{ task }}</p>
-    <button class="btn btn-info">Another Request</button>
+    <button class="btn btn-info"
+    (click)="onAnotherReq()">Another Request</button>
   </div>`,
   styleUrls: ['./flow-wt.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class FlowWtComponent implements OnChanges {
+
+  showTechDisplay = false;
+
+  onAnotherReq() {
+    this.showTechDisplay = true;
+  }
+
   ngOnChanges(changes: SimpleChanges): void {
     console.log('flow updated!');
   }
