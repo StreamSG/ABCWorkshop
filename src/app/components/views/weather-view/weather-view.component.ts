1dd06e9aebc8e352baa6b925064e1afe4fe654cc
looksliketyler <91300303+looksliketyler@users.noreply.github.com>
2022-12-20 09:13:13 -0500

    commit to pull on other computer

--- src/app/components/views/weather-view/weather-view.component.ts
+++ src/app/components/views/weather-view/weather-view.component.ts
@@ -3,7 +3,7 @@ import { Subject, takeUntil, take } from 'rxjs';
 
 import { WeatherAlertInfo, WeatherAlertResponse } from 'src/app/models/weather-alert.model';
 import { WeatherService } from 'src/app/services/weather.service';
-
+import * as SafetyJobAids from '../../../safetyJobAids.json';
 
 @Component({
   selector: 'app-weather-view',
@@ -13,21 +13,24 @@ import { WeatherService } from 'src/app/services/weather.service';
 export class WeatherViewComponent implements OnInit, OnDestroy {
   public weatherAlertResponse: WeatherAlertResponse;
   private ngUnsubscribe = new Subject<void>();
+  private safetyJobAids: object;
 
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
     this.subscribeToWeatherService();
+    this.safetyJobAids = SafetyJobAids.default;
   }
 
   ngOnDestroy(): void {
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete
   }
-/**
-* @description - Subscribes to weather service and sets global variable for the api response
-* @returns {void}
-*/
+
+  /**
+   * @description - subscribes to weather service and gets api result
+   * @returns {void}
+   */
   private subscribeToWeatherService(): void {
     this.weatherService.getLoading().pipe(take(2), takeUntil(this.ngUnsubscribe)).subscribe({
        next: (loading: boolean) => {
@@ -36,6 +39,30 @@ export class WeatherViewComponent implements OnInit, OnDestroy {
          }
        }
      });
+  }
+
+  public doesWeatherAlertRelateToSafetyJobAid(weatherAlert: WeatherAlertInfo): boolean {
+    const coldWeatherTags: string[] = ["Ice", "Cold", "Snow", "Winter", "Wind", "Freez", "Blizzard", "Frost"]; // spelling of 'freez' is intentional as the event name from api may say 'freeze' or 'freezing'
+    if (Object.keys(weatherAlert) && weatherAlert.event) {
+      Object.keys(this.safetyJobAids).forEach(tag => {
+        if (weatherAlert.event.indexOf(tag) > -1) {
+          console.log(tag)
+          console.log(this.safetyJobAids[tag])
+        } else
+        for (let coldTag of coldWeatherTags) {
+          if (weatherAlert.event.indexOf(coldTag) > -1) {
+            console.log('cold weather man')
+            break
+          }
+        }
+      })
+      return true
+      // if (this.safetyJobAids.indexOf(weatherAlert.event) > -1) {
+      //   console.log('ok')
+      // }
+    } else {
+      return false;
+    }
   }
 
 }
