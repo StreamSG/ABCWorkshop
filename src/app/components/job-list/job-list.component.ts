d263477a98ed49002e53db9256d48e7e118e1426
Tyler Regan <tr284v@att.com>
2022-12-22 00:01:00 -0500

    added jsdoc param

--- src/app/components/job-list/job-list.component.ts
+++ src/app/components/job-list/job-list.component.ts
@@ -24,6 +24,7 @@ export class JobListComponent implements OnInit {
   /**
    * @description On click method for use in html for when a job from the list is clicked, in order to set the selected job index and route to the job view.
    * @param {number} index The index of the job to mark as selected
+   * @param {JobData} selectedJob - the current job clicked, lat long needed for this job
    * @returns {void} Returns void {void}
    */
   public onJobClicked(selectedJob: JobData, index: number): void {
