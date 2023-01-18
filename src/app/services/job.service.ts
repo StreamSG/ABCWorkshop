352ea1d2c90a4f549133949a608bc8c3955e13c6
micah.wehrle <micah23@gmail.com>
2023-01-18 10:09:13 -0500

    timer bar coloring

--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -10,7 +10,8 @@ export class JobService {
   // TODO - Pick locations that will have alerts prior to meeting!
   private jobs: JobData[] = [
     new JobData(41.4993, -81.6944, 'Install', 'Cleveland, OH'),
-    new JobData(44.0805, -103.2310, 'Repair', 'Rapid City, SD'),
+    // new JobData(44.0805, -103.2310, 'Repair', 'Rapid City, SD'),
+    new JobData(48.4779, -120.1862, 'Repair', 'Winthrop, WA'),
     new JobData(30.2672, -97.7431, 'Helper', 'Austin, TX'),
     new JobData(26.1224, -80.1373, 'BSW', 'Fort Something, FL'),
     new JobData(34.0195, -118.4912, 'POTS', 'Santa Monica, CA')
