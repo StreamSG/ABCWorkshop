d0b28bd7fa6d4127e46aebf46ff316ce4ab9b980
micah.wehrle <micah23@gmail.com>
2023-06-01 22:16:05 -0400

    MenuItem changed type to interface per standard jsdoc'd MenuItem

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  // Sidebar menu items to be dynamically generated from the array below. See MenuItem documentation for implementation help. Far future feature: convert to class and add service file for more advanced control over sidebar elements (such as disabling and enabling at will).
   public menuItems: MenuItem[] = [
     { text: 'Home', routerLink: '' },
     { text: 'Alerts', routerLink: 'weather' },
@@ -33,18 +34,31 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void { }
 }
 
-export type MenuItem = {
+/**
+ * @description For use with the header sidebar menu. Allows the creation of an array of objects to generate the sidebar links
+ */
+export interface MenuItem {
+  /** @property Greys out sidebar elements so they cannot be clicked */ 
   disabled?: boolean;
+  /** @property The text to be displayed in the sidebar */
   text?: string;
+  /** @property The path to route to when clicked */
   routerLink?: string;
+  /** @property Image data for displaying at the end of the menu line item */
   img?: {
+    /** @property Asset path for the image */
     path: string;
+    /** @property Alt text for the image */
     alt?: string;
+    /** @property Width of the image */
     width?: string;
+    /** @property Height of the image */
     height?: string;
   };
+  /** @property Allows for a dropdown menu of more MenuItems. Cannot be doubly nested. */
   dropdown?: MenuItem[];
+  /** @property I found that the callback function is technically executed after the router loads the new page */
   callback?: Function;
-  /** * @property isHr should only be used by itself, as all other properties will be ignored when isHr is present */
+  /** @property isHr should only be used by itself, as all other properties will be ignored when isHr is present */
   isHr?: true; 
 }
